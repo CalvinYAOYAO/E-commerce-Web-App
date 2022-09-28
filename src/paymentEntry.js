927ca42dca72a406cf80f269ed9cc0be931b6c99
Zheng Du <du.913@osu.edu>
2022-09-28 19:26:52 -0400

    Foundation by Lab-4 instructions

---
+++ src/paymentEntry.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const paymentEntry = () => {
+    let title = "paymentEntry page";
+
+    return (
+        <div>
+            <h1>
+                {title}
+            </h1>
+        </div>
+    );
+}
+
+export default paymentEntry;
---
+++ src/purchase.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const purchase = () => {
+    let title = "Purchase page";
+    console.log("HERE")
+    return (
+        <div>
+            <h1>
+                {title}
+            </h1>
+        </div>
+    );
+}
+
+export default purchase;
