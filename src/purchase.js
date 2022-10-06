42801edb2f0546b26d663c95497bbcf2a36092d4
JacobSlivka <46791911+JacobSlivka@users.noreply.github.com>
2022-10-06 15:25:53 -0400

    Finished styling purchase page

--- src/purchase.js
+++ src/purchase.js
@@ -1,50 +1,63 @@
 import React, {useState} from "react";
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './custom.css';
 
 const purchase = (props) => {
-    let title = "Purchase page";
-    const { products, onAddToCart, onQuantityChanged, productQuantities} = props; // this is an array of products from the data.js file
+    const {products, onAddToCart, onQuantityChanged, productQuantities} = props; // this is an array of products from the data.js file
     return (
         <div>
-            <h1>
-                {title}
-            </h1>
+            <h2>
+                Available Products
+            </h2>
 
-            <tr>
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
 
-                <th>Picture</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Add to cart</th>
-            </tr>
+                    <th>Picture</th>
+                    <th class={"text-start"}>Name</th>
+                    <th class={"text-start"}>Description</th>
+                    <th class={"text-start"}>Price</th>
+                    <th>Quantity</th>
+                    <th>Add to cart</th>
+                </tr>
+                </thead>
 
-            {/* for each product in the products array make a row in the table for the
-            product {product.id in productQuantities && productQuantities[product.id] === 1 ? selected : ""}*/
-            }
-            {products.map( (product) => {
-                return (
-                    <tr key={product.id}>
-                        <td> <img src={product.image} alt={product.name} width="50"
-                                  height="50"></img> </td>
-                        <td>{product.name}</td>
-                        <td>{product.description}</td>
-                        <td>{product.price}</td>
-                        <td><select name={"Quantity" + product.id} id={"Quantity" + product.id} onChange={event => onQuantityChanged(product.id,event.target.value)}
-                        defaultValue={product.id in productQuantities ? productQuantities[product.id] : 1}>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select></td>
-                        <th> <button type="button" class="btn btn-secondary" onClick={() => onAddToCart(product)} >Add to cart</button></th>
-                    </tr>
-                );
+                {/* for each product in the products array make a row in the table for the
+            product*/}
+                <tbody>
+                {products.map((product) => {
+                    return (
+                        <tr key={product.id}>
+                            <td><img class={"img-thumbnail"} src={product.image} alt={product.name} width="100" ></img></td>
+                            <td class={"text-start"}>{product.name}</td>
+                            <td class={"text-start"}> {product.description}</td>
+                            <td class={"text-start"}>{product.price}</td>
+                            <td><select class={"selectwidthauto form-select"} name={"Quantity" + product.id}
+                                        id={"Quantity" + product.id}
+                                        onChange={event => onQuantityChanged(product.id, event.target.value)}
+                                        defaultValue={product.id in productQuantities ? productQuantities[product.id] : 1}>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select></td>
+                            <th>
+                                <button class={"btn btn-primary"} onClick={() => onAddToCart(product)}>Add to cart
+                                </button>
+                            </th>
+                        </tr>
+                    );
 
-            })}
+                })}
+                </tbody>
 
+            </table>
         </div>
+
     );
 }
 
