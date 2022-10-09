957bbe66af83bab659f0fd780935065bd95c7860
Zheng Du <du.913@osu.edu>
2022-10-09 17:13:11 -0400

    Home and About page

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,49 @@
 import React from "react";
+import classes from "./about.module.css";
 
 const About = () => {
   
   return (
-    <div>
-      <h1>
-        about page
-      </h1>
+    
+    <div class="card-group">
+  <div class="card">
+    <img src="img/4.png" class="card-img-top" alt="img"/>
+    <div class="card-body">
+      <h3 class="card-title">Zheng Du</h3>
+      <h5 class="card-title">Founder</h5>
+      <p class="card-text">Out mission is to display all the best mobile phones you cannot possibly purchase, since we are still in process to .</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
+  </div>
+  <div class="card">
+    <img src="img/5.png" class="card-img-top" alt="img"/>
+    <div class="card-body">
+      <h3 class="card-title">Jacob Slivka</h3>
+      <h5 class="card-title">CEO</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="img/3.png" class="card-img-top" alt="img"/>
+    <div class="card-body">
+      <h3 class="card-title">Yongyao Huang</h3>
+      <h5 class="card-title">CFO</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="img/2.png" class="card-img-top" alt="img"/>
+    <div class="card-body">
+      <h3 class="card-title">Andy Yu</h3>
+      <h5 class="card-title">CTO</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
   );
 };
   
