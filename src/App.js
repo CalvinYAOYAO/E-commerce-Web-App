18c3a328e473dd9687d81c1f20257b6f269886ea
JacobSlivka <46791911+JacobSlivka@users.noreply.github.com>
2022-10-17 18:54:40 -0400

    Front-end get products from inventory microservice

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import { Toast } from 'bootstrap';
 
 
 function App() {
-  const { products } = data;
+
   //set cart items is a function to change the value of cart items
   //cart items is an array with all the items in the cart
   const [cartItems, setCartItems] = useState([])
@@ -67,7 +67,7 @@ function App() {
               <Route path='/about' element={<About />} />
               <Route path='/cart' element={<Cart cartItems = {cartItems}/>} />
 
-              <Route path='/purchase' element={<Purchase onAddToCart={onAddToCart} onQuantityChanged={onQuantityChanged} products={products} productQuantities={productQuantities}/>} />
+              <Route path='/purchase' element={<Purchase onAddToCart={onAddToCart} onQuantityChanged={onQuantityChanged} productQuantities={productQuantities}/>} />
               <Route path='/' element={<Navigate replace to="/purchase" />} />
               <Route path='/purchase/paymentEntry' element={<PaymentEntry />} />
               <Route path='/purchase/shippingEntry' element={<ShippingEntry />} />
--- src/purchase.js
+++ src/purchase.js
@@ -3,16 +3,21 @@ import './custom.css';
 import StepsBar from "./components/StepsBar";
 
 import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 const Purchase = (props) => {
-    const {products, onAddToCart, onQuantityChanged, productQuantities} = props; // this is an array of products from the data.js file
-    
+    const {onAddToCart, onQuantityChanged, productQuantities} = props; // this is an array of products from the data.js file
+    const [products, setProducts] = useState([])
     let navigate = useNavigate();
 
     async function handleContinue(event) {
         navigate('/cart', {replace: true});
     }
-    
+
+    fetch("https://57bg18w306.execute-api.us-east-2.amazonaws.com/v1/inventory/")
+        .then( response => response.json() )
+        .then( data => setProducts(data.inventory) )
+
     return (
         <div>
             <StepsBar curStep = {0}/>
@@ -38,28 +43,36 @@ const Purchase = (props) => {
             product*/}
                 <tbody>
                 {products.map((product) => {
-                    return (
-                        <tr key={product.id}>
-                            <td><img class={"img-thumbnail"} src={product.image} alt={product.name} width="100" ></img></td>
-                            <td class={"text-start"}>{product.name}</td>
-                            <td class={"text-start"}> {product.description}</td>
-                            <td class={"text-start"}>${product.price}</td>
-                            <td><select class={"selectwidthauto form-select"} name={"Quantity" + product.id}
-                                        id={"Quantity" + product.id}
-                                        onChange={event => onQuantityChanged(product.id, event.target.value)}
-                                        defaultValue={product.id in productQuantities ? productQuantities[product.id] : 1}>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select></td>
-                            <th>
-                                <button class={"btn btn-primary"} data-bs-toggle="modal" data-bs-target="#confirmModal" onClick={() => onAddToCart(product)}>Add to cart
-                                </button>
-                            </th>
-                        </tr>
-                    );
+                    if (product.stock>0) {
+                        return (
+                            <tr key={product.id}>
+                                <td><img class={"img-thumbnail"} src={product.image} alt={product.name}
+                                         width="100"></img></td>
+                                <td class={"text-start"}>{product.name}</td>
+                                <td class={"text-start"}> {product.desc}</td>
+                                <td class={"text-start"}>${product.price}</td>
+                                <td><select class={"selectwidthauto form-select"} name={"Quantity" + product.id}
+                                            id={"Quantity" + product.id}
+                                            onChange={event => onQuantityChanged(product.id, event.target.value)}
+                                            defaultValue={product.id in productQuantities ? productQuantities[product.id] : 1}>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select></td>
+                                <th>
+                                    <button class={"btn btn-primary"} onClick={() => onAddToCart(product)}>Add
+                                        to cart
+                                    </button>
+                                </th>
+                            </tr>
+                        );
+                    }
+                    else
+                    {
+                        return ""
+                    }
 
                 })}
                 </tbody>
@@ -69,24 +82,6 @@ const Purchase = (props) => {
             <button type="button" className="btn btn-primary" onClick={handleContinue}>
                 Continue
             </button>
-
-            <div class="modal fade" id="confirmModal" tabIndex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="confirmModalLabel">Added to cart</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    A new phone has been added to the cart, view cart & checkout?
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Continue Shopping</button>
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={handleContinue}>Checkout</button>
-                </div>
-                </div>
-            </div>
-            </div>
 
         </div>
 
