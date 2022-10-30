b766ba422ccf549cfb3e71da576b6cd77e1bc54c
CalvinYAOYAO <13168495217@163.com>
2022-10-30 06:57:17 -0400

    Add source code of orderProcessing microservice

---
+++ lambda_orderProcess/index.js
@@ -0,0 +1,60 @@
+'use strict';
+console.log('Loading orderProcessing microservice');
+
+const axios = require('axios');
+const storeData = require('./storeData');
+
+exports.handler = async (event) => {
+    // var list = [
+    //         {id: 1,
+    //         quantity: 1},
+    //         {id: 2,
+    //         quantity: 2}
+    //     ];
+
+    var body = JSON.parse(event.body);
+    var order = body.order;
+    var checkStock = (inventory, order) => {
+        var items = order.items;
+        var quantities = order.quantities;
+        for (var i in items) {
+            var itemId = items[i];
+            var product = inventory.find(obj => obj.id === itemId);
+            if (quantities[i] > product.stock) return false;
+        }
+        return true;
+    }
+
+    var responseBody;
+    await axios.get('https://57bg18w306.execute-api.us-east-2.amazonaws.com/v1/inventory').then(res => {
+        if (checkStock(res.data.inventory, order)) {
+            storeData(order);
+            responseBody = {
+                isValid: true,
+                message: "Order successfully！",
+                confirmNum: "ABC123456"
+            }
+        }else {
+            responseBody = {
+                isValid: false,
+                message: "Error: Inventory shortage!"  
+            }
+        }
+        // // to see how the event looks like
+        // responseBody.input = event;
+    });
+    
+    let response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header" : "my custom header value",
+            // CORS policy: allow access from all domain region
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        body: JSON.stringify(responseBody)
+    };
+    
+    return response
+};
